7761d69a2bead08f78090db04a066e479e234ff6
AJLambda <apj2277@gmail.com>
2019-05-17 09:24:31 -0700

    configured database

---
+++ games/gamesModel.js
@@ -0,0 +1,32 @@
+const db = require("../data/dbConfig.js");
+
+module.exports = {
+  insert,
+  update,
+  remove,
+  getAll,
+  findById
+};
+
+async function insert(games) {
+  const [id] = await db("games").insert(game, "id"); // [1], 'id' is for heroku
+  return db("games")
+    .where({ id })
+    .first();
+}
+
+async function update(id, changes) {
+  return null;
+}
+
+function remove(id) {
+  return null;
+}
+
+function getAll() {
+  return db("games");
+}
+
+function findById(id) {
+  return null;
+}

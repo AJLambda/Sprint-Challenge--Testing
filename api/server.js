cdeac704a57073f2ba7e54418be0923cfc8669c0
AJLambda <apj2277@gmail.com>
2019-05-17 09:44:06 -0700

    created test db

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const games = require("../games/gamesModel.js");
+const db = require("../data/dbConfig");
 
 const server = express();
 
@@ -14,6 +15,23 @@ server.get("/games", async (req, res) => {
   const rows = await games.getAll();
 
   res.status(200).json(rows);
+});
+
+server.post("/games", async (req, res) => {
+  try {
+    if (req.body.genre && req.body.title) {
+      const game = await db.insert(req.body);
+      res.status(200).json(game);
+    } else {
+      res.status(422).json({
+        message: "missing title and genre to create game"
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "error creating game"
+    });
+  }
 });
 
 module.exports = server;
